09cabe11196315bede654eae93c6f05fddacd8c1
Debora Serra <debora.r.serra@gmail.com>
2025-10-04 13:56:06 -0700

    fix: remove random mode toggle

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,8 @@
-import { ModeToggle } from "@/components/themeProvider/mode-toggle";
 import Login from "../components/Login";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-main">
-      <ModeToggle />
+    <div className='flex flex-col min-h-screen items-center justify-center bg-main'>
       <Login />
     </div>
   );
